d81cacd7ad19b1cb414398b150c985a8ac664f0b
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-01-14 21:42:20 +0530

    final

--- script.js
+++ script.js
@@ -27,6 +27,16 @@ function init() {
 init()
 
 
+var crsr = document.querySelector("#cursor");
+var main = document.querySelector("#main");
+
+main.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x +20 +"px"
+    crsr.style.top = dets.y+ 20 +"px"
+})
+
+
+
 var tl = gsap.timeline({
     scrollTrigger:{
         trigger:"#page1 h1",
@@ -64,3 +74,36 @@ var tl2 = gsap.timeline({
 tl2.to("#main",{
     backgroundColor:"#fff",
 })
+
+var tl3 = gsap.timeline({
+    scrollTrigger:{
+        trigger:"#page1 h1",
+        scroller:"#main",
+        start:"top -280%",
+        end:"top -300%",
+        scrub:3
+    }
+})
+tl3.to("#main",{
+    backgroundColor:"#0F0D0D"
+})
+
+
+var boxes = document.querySelectorAll(".box")
+boxes.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        var att = elem.getAttribute("data-image")
+        crsr.style.width = "470px"
+        crsr.style.height = "370px"
+        crsr.style.borderRadius = "0"
+        crsr.style.backgroundImage = `url(${att})`
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.style.backgroundColor = "transparent"
+        crsr.style.width = "20px"
+        crsr.style.height = "20px"
+        crsr.style.borderRadius = "50%"
+        crsr.style.backgroundImage = `none`
+    })
+})
+
